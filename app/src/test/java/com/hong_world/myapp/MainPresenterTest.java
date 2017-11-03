558e8b310aa322250d86ce90be3d27485f697eeb
yanhong <yanhong@sztenda.cn>
2017-11-03 18:09:24 +0800

    first

---
+++ app/src/test/java/com/hong_world/myapp/MainPresenterTest.java
@@ -0,0 +1,16 @@
+package com.hong_world.myapp;
+
+import com.hong_world.myapp.modle.TasksRepository;
+
+import org.mockito.Mock;
+
+/**
+ * Date: 2017/11/3.18:01
+ * Author: hong_world
+ * Description:
+ * Version:
+ */
+public class MainPresenterTest {
+    @Mock
+    private TasksRepository mTasksRepository;
+}
