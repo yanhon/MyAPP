a7f1363e2ac20d37f0c06df60b1e906c66dd6da9
yanhong <yanhong@sztenda.cn>
2017-11-21 11:38:03 +0800

    home模块独立运行

--- homemodle/src/debug/java/com/hong_world/homemodle/debug/HomeDebugAppliction.java
+++ homemodle/src/debug/java/com/hong_world/homemodle/debug/HomeDebugAppliction.java
@@ -1,4 +1,4 @@
-package com.hong_world.myapp;
+package com.hong_world.homemodle.debug;
 
 import android.app.Application;
 
@@ -8,33 +8,22 @@ import com.hong_world.library.view.status.callback.EmptyCallback;
 import com.hong_world.library.view.status.callback.ErrorCallback;
 import com.hong_world.library.view.status.callback.LoadingCallback;
 import com.hong_world.library.view.status.callback.TimeoutCallback;
-import com.hong_world.myapp.bean.DaoMaster;
-import com.hong_world.myapp.bean.DaoSession;
 import com.kingja.loadsir.core.LoadSir;
 
-import org.greenrobot.greendao.database.Database;
-
 /**
- * Date: 2017/11/13.17:32
+ * Date: 2017/11/21.10:26
  * Author: hong_world
  * Description:
  * Version:
  */
 
-public class MyApplication extends Application {
-    /**
-     * A flag to show how easily you can switch from standard SQLite to the encrypted SQLCipher.
-     */
-    public static final boolean ENCRYPTED = true;
-    private static DaoSession daoSession;
-
+public class HomeDebugAppliction extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        initILoaderManager();
-        initGreenDao();
-        initViewStatus();
         initArouter();
+        initILoaderManager();
+        initViewStatus();
     }
 
     private void initArouter() {
@@ -56,19 +45,8 @@ public class MyApplication extends Application {
                 .commit();
     }
 
-    private void initGreenDao() {
-        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, ENCRYPTED ? "notes-db-encrypted" : "notes-db");
-        Database db = ENCRYPTED ? helper.getEncryptedWritableDb("super-secret") : helper.getWritableDb();
-        daoSession = new DaoMaster(db).newSession();
-    }
-
     private void initILoaderManager() {
 //        ILoaderManager.setLoader(new FrescoLoader());//外部定制图片加载库Fresco
 //        ILoaderManager.getLoader().init(this);
     }
-
-    public static DaoSession getDaoSession() {
-        return daoSession;
-    }
-
 }
