62370424f1a93ddd2b7753d4f09e8724e6408f3e
hong <hong_world>
2018-11-22 15:26:52 +0800

    1、文件上传显示进度

---
+++ common/src/main/java/com/hong_world/common/net/upload/UploadProgressCallBack.java
@@ -0,0 +1,18 @@
+package com.hong_world.common.net.upload;
+
+/**
+ * Date: 2018/11/22. 10:34
+ * Author: hong_world
+ * Description:
+ * Version:
+ */
+public interface UploadProgressCallBack {
+    /**
+     * 回调进度
+     *
+     * @param bytesWritten  当前读取响应体字节长度
+     * @param contentLength 总长度
+     * @param done          是否读取完成
+     */
+    void onResponseProgress(long bytesWritten, long contentLength, boolean done);
+}
